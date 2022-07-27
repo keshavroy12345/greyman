b6f142e994826c552d76e2716dd71ddfa521ffe6
Keshavroy12345 <Keshu1123@gmail.com>
2022-07-27 13:12:21 +0530

    tfufuj

---
+++ newProjectw/src/test/java/PracticeTest/Prcticeztest.java
@@ -0,0 +1,12 @@
+package PracticeTest;
+
+import org.testng.annotations.Test;
+
+public class Prcticeztest {
+	@Test
+	public void display() {
+		System.out.println("krishnam");
+		System.out.println("keshav");
+	}
+
+}
